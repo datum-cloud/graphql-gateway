103ce8b86f39aefcdce4150452a8b49ccd536b72
Jose Szychowski <joseszychowski@gmail.com>
2025-11-28 15:38:19 -0300

    chore: Comment out cache and responseCaching configurations in gateway.config.ts

--- gateway.config.ts
+++ gateway.config.ts
@@ -2,17 +2,17 @@ import { defineConfig } from '@graphql-hive/gateway'
  
 export const gatewayConfig = defineConfig({
   logging: (process.env.LOGGING || 'info') as 'debug' | 'info' | 'warn' | 'error',
-  cache: {
-    type: 'localforage',
-    driver: ['LOCALSTORAGE'],
-    name: 'DatumGraphQLGateway',
-    version: 1.0, 
-    size: 4980736, 
-    storeName: 'keyvaluepairs',
-    description: 'Cache storage for Datum GraphQL Gateway', 
-  },
-  responseCaching: {
-    session: request => request.headers.get('authentication'), // cache based on the authentication header
-    ttl: 1000 * 60 * 2, // 2 minutes
-  }
+//  cache: {
+//    type: 'localforage',
+//    driver: ['LOCALSTORAGE'],
+//    name: 'DatumGraphQLGateway',
+//    version: 1.0, 
+//    size: 4980736, 
+//    storeName: 'keyvaluepairs',
+//    description: 'Cache storage for Datum GraphQL Gateway', 
+//  },
+//  responseCaching: {
+//    session: request => request.headers.get('authentication'), // cache based on the authentication header
+//    ttl: 1000 * 60 * 2, // 2 minutes
+//  }
 })
